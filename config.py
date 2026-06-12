68ba8698f5c8da3d2492dc4ead852b6c88449b56
Aadi639707 <aadi639707@gmail.com>
2026-06-12 12:59:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_HASH = "0284b25e838bca33b1f044460125799f"     # Apna API Hash yahan dalein (
 SESSION_STRING = "BQJFVdAAoKxGl4zBAKm1Pr5yJiwuIpkc1trOfExt34cYk1wEyT62H8wVMeky6AH6DIBLKK7mFQOM5w7ZmFCwZOy5PzEFdhNQP--weql9l7VNRevgK--bQQQZ0zR3iRE2TSoBiLpuwcy3mqimnFMDEm9bCcz9ZAWudru1merTJ0EtGgOlrv_ABJDQgJzuorob65uYiVPkW5MetPge6gR9pXaRhzrtxjxoxvjt08vcBX7YSB_Q982TEf0BBr7B5D_IAeEo9eA_3D-HWOyJq-qJE-pX0MwmyP_BYb2nRpEvOFhAJ2T1QPGp4go-8ACVLO4jtttG5mMjox9V5lCP-7LjKZKqCKaHMQAAAAGpED7oAA"# Apna Pyrogram Session String yahan dalein
 
 # AI Credentials
-GROQ_API_KEY = "your_groq_api_key" # Groq Console se free key lekar yahan dalein
+GROQ_API_KEY = "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" # Groq Console se free key lekar yahan dalein
 
 # Bot Settings
 COMMAND_PREFIX = "."
