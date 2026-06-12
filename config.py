a6539c5a11b193e873f8d1ce05f8eadd3491abb6
Aadi639707 <aadi639707@gmail.com>
2026-06-12 12:57:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 # Telegram Credentials
-API_ID = 1234567               # Apna API ID yahan dalein (Integer)
-API_HASH = "your_api_hash"     # Apna API Hash yahan dalein (String)
-SESSION_STRING = "your_session"# Apna Pyrogram Session String yahan dalein
+API_ID = 38098384               # Apna API ID yahan dalein (Integer)
+API_HASH = "0284b25e838bca33b1f044460125799f"     # Apna API Hash yahan dalein (String)
+SESSION_STRING = "BQJFVdAAoKxGl4zBAKm1Pr5yJiwuIpkc1trOfExt34cYk1wEyT62H8wVMeky6AH6DIBLKK7mFQOM5w7ZmFCwZOy5PzEFdhNQP--weql9l7VNRevgK--bQQQZ0zR3iRE2TSoBiLpuwcy3mqimnFMDEm9bCcz9ZAWudru1merTJ0EtGgOlrv_ABJDQgJzuorob65uYiVPkW5MetPge6gR9pXaRhzrtxjxoxvjt08vcBX7YSB_Q982TEf0BBr7B5D_IAeEo9eA_3D-HWOyJq-qJE-pX0MwmyP_BYb2nRpEvOFhAJ2T1QPGp4go-8ACVLO4jtttG5mMjox9V5lCP-7LjKZKqCKaHMQAAAAGpED7oAA"# Apna Pyrogram Session String yahan dalein
 
 # AI Credentials
 GROQ_API_KEY = "your_groq_api_key" # Groq Console se free key lekar yahan dalein
